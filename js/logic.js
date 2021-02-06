a08443646c94efcb05213a2f744025a25de164e1
AhmedAtef <ahmaadatef3@gmail.com>
2021-02-06 21:34:02 +0200

    conflict resolved

--- js/logic.js
+++ js/logic.js
@@ -20,152 +20,153 @@ southPipeImage.src = "assets/img/sp.png";
 pipesGap = 150;
 pipesSpeed = 1;
 distanceBetweenPipes = 250
-let planetGravity=1.25;
-let dragonImg=new Image();
+let planetGravity = 1.25;
+let dragonImg = new Image();
 let drakeFPS = parseInt(localStorage.dragonFrames);
 let drakeSrc = localStorage.dragon;
 console.log(drakeFPS);
 
 //dragon object
 const drake = {
-    x:0,
-    y:200,
-    w:90,
-    h:90,
-    index:0,
-    draw: function() {
-       
-            dragonImg.src=`${drakeSrc}/${this.index}.png`;
-           
-        
+    x: 0,
+    y: 200,
+    w: 90,
+    h: 90,
+    index: 0,
+    draw: function () {
+
+        dragonImg.src = `${drakeSrc}/${this.index}.png`;
+
+
         // dragonImg.src=`assets/img/0-dragon/${this.index}.png`;
         ctx.drawImage(dragonImg, this.x, this.y, this.w, this.h);
-         
-},
-    update:function(){
-        if(frames%10==0){
-        if(this.index<drakeFPS){
-            this.index++;
-        }else{
-            this.index=0;
-        }
-        this.gravity();
 
     },
-    gravity:function(){
-        if(this.y)
-        this.y+=planetGravity;
-
-    },  
-    moveUp:function(){
-        if(this.y-50>0){
-           this.y-=50;
+    update: function () {
+        if (frames % 10 == 0) {
+            if (this.index < drakeFPS) {
+                this.index++;
+            } else {
+                this.index = 0;
+            }
+            this.gravity();
         }
-    }
+    },
 
-}
+        gravity: function() {
+            if (this.y)
+                this.y += planetGravity;
+
+        },
+        moveUp: function() {
+            if (this.y - 50 > 0) {
+                this.y -= 50;
+            }
+        }
+
+    }
 
 //background object
 const bg = {
-    x: 0,
-    y: 0,
-    w: 450,
-    h: 600,
+        x: 0,
+        y: 0,
+        w: 450,
+        h: 600,
 
-    draw: function () {
-        ctx.drawImage(bgImage, this.x, this.y, this.w, this.h);
-    }
-};
-
-//first foreground object under pipes
-const fg1 = {
-    x: 0,
-    y: 262,
-    w: 900,
-    h: 209,
-    dx: 2,
-
-    draw: function () {
-        ctx.drawImage(fg1Image, this.x, this.y, this.w, this.h);
-    },
-
-    update: function () {
-        this.x = (this.x - this.dx) % (this.w / 2);
-    }
-
-};
-
-//second foreground object above pipes
-const fg2 = {
-    x: 0,
-    y: 459,
-    w: 900,
-    h: 141,
-    dx: 2,
-
-    draw: function () {
-        ctx.drawImage(fg2Image, this.x, this.y, this.w, this.h);
-    },
-
-    update: function () {
-        this.x = (this.x - this.dx) % (this.w / 2);
-    }
-
-};
-
-//Pipes object
-const pipes = {
-    position: [],
-    maxYPos: -260,
-    w: 75,
-    h: 561,
-    gap: pipesGap,
-    dx: pipesSpeed,
-
-    draw: function () {
-        for (let i = 0; i < this.position.length; i++) {
-            let p = this.position[i];
-            let northY = p.y;
-            let southY = northY + this.h + this.gap;
-            //Draw north pipe
-            ctx.drawImage(northPipeImage, p.x, northY, this.w, this.h);
-            //Draw south pipe
-            ctx.drawImage(southPipeImage, p.x, southY, this.w, this.h);
+        draw: function () {
+            ctx.drawImage(bgImage, this.x, this.y, this.w, this.h);
         }
-    },
+    };
 
-    update: function () {
-        //Create new pipes with random y position
-        if (frames % distanceBetweenPipes === 0) {
-            this.position.push({
-                x: cvs.width,
-                // y: Math.floor((Math.random() * this.maxYPos) + 1)
-                y: this.maxYPos * (Math.random() + 1)
-            })
+    //first foreground object under pipes
+    const fg1 = {
+        x: 0,
+        y: 262,
+        w: 900,
+        h: 209,
+        dx: 2,
+
+        draw: function () {
+            ctx.drawImage(fg1Image, this.x, this.y, this.w, this.h);
+        },
+
+        update: function () {
+            this.x = (this.x - this.dx) % (this.w / 2);
         }
 
-        for (let i = 0; i < this.position.length; i++) {
-            let p = this.position[i];
+    };
 
-            //Move pipes to the left
-            p.x -= this.dx;
-            let bottomPipeYPos = p.y + this.h + this.gap;
+    //second foreground object above pipes
+    const fg2 = {
+        x: 0,
+        y: 459,
+        w: 900,
+        h: 141,
+        dx: 2,
 
-            
+        draw: function () {
+            ctx.drawImage(fg2Image, this.x, this.y, this.w, this.h);
+        },
+
+        update: function () {
+            this.x = (this.x - this.dx) % (this.w / 2);
+        }
+
+    };
+
+    //Pipes object
+    const pipes = {
+        position: [],
+        maxYPos: -260,
+        w: 75,
+        h: 561,
+        gap: pipesGap,
+        dx: pipesSpeed,
+
+        draw: function () {
+            for (let i = 0; i < this.position.length; i++) {
+                let p = this.position[i];
+                let northY = p.y;
+                let southY = northY + this.h + this.gap;
+                //Draw north pipe
+                ctx.drawImage(northPipeImage, p.x, northY, this.w, this.h);
+                //Draw south pipe
+                ctx.drawImage(southPipeImage, p.x, southY, this.w, this.h);
+            }
+        },
+
+        update: function () {
+            //Create new pipes with random y position
+            if (frames % distanceBetweenPipes === 0) {
+                this.position.push({
+                    x: cvs.width,
+                    // y: Math.floor((Math.random() * this.maxYPos) + 1)
+                    y: this.maxYPos * (Math.random() + 1)
+                })
+            }
+
+            for (let i = 0; i < this.position.length; i++) {
+                let p = this.position[i];
+
+                //Move pipes to the left
+                p.x -= this.dx;
+                let bottomPipeYPos = p.y + this.h + this.gap;
 
 
-             
-            //Delete pipes that reached the end of canvas from array
-            if (p.x + this.w <= 0) {
-                this.position.shift();
-                //Increment Score
-                score.value += 1;
-                score.best = Math.max(score.value, score.best);
-                localStorage.setItem("best", score.best);
+
+
+
+                //Delete pipes that reached the end of canvas from array
+                if (p.x + this.w <= 0) {
+                    this.position.shift();
+                    //Increment Score
+                    score.value += 1;
+                    score.best = Math.max(score.value, score.best);
+                    localStorage.setItem("best", score.best);
+                }
             }
         }
     }
-}
 
 //South pipe object
 
@@ -174,30 +175,30 @@ const pipes = {
 //Score
 const score = {
 
-    best: parseInt(localStorage.getItem("best")) || 0,
-    value: 0,
+        best: parseInt(localStorage.getItem("best")) || 0,
+        value: 0,
 
-    draw: function () {
+        draw: function () {
 
-        ctx.fillStyle = "#FFF";
-        ctx.strokeStyle = "#000";
+            ctx.fillStyle = "#FFF";
+            ctx.strokeStyle = "#000";
 
-        ctx.lineWidth = 2;
-        ctx.font = "35px Teko";
-        ctx.fillText(this.value, cvs.width / 2, 50);
-        ctx.strokeText(this.value, cvs.width / 2, 50);
+            ctx.lineWidth = 2;
+            ctx.font = "35px Teko";
+            ctx.fillText(this.value, cvs.width / 2, 50);
+            ctx.strokeText(this.value, cvs.width / 2, 50);
+
+        }
 
     }
 
-}
-
 //Draw all canvas elements
 function draw() {
-    bg.draw();
-    fg1.draw();
-    drake.draw();
-    pipes.draw();
-    fg2.draw();
+        bg.draw();
+fg1.draw();
+drake.draw();
+pipes.draw();
+fg2.draw();
 }
 
 function update() {
