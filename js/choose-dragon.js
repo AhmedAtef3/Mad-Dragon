befe88e9a3e3db965e8c57dcd274577163a13018
HatemHashem <hatemsayed001@gmail.com>
2021-02-09 23:56:55 +0200

    add audio and  character movent keys

--- js/choose-dragon.js
+++ js/choose-dragon.js
@@ -1,3 +1,4 @@
+window.onload=function(){
 let images = document.getElementsByTagName("img");
 images[0].addEventListener("click", function() {
     localStorage.setItem("dragon","assets/img/0-dragon");
@@ -11,3 +12,7 @@ images[2].addEventListener("click", function() {
     localStorage.setItem("dragon","assets/img/2-dragon");
     localStorage.setItem("dragonFrames","4");
 });
+let bgMusic=new Audio();
+bgMusic.src="assets/audio/background.mpeg";
+bgMusic.play();
+}
